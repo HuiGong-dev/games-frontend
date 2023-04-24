718905e5ab9fc723ae425cf0b68ba152144b0966
HuiGong-dev <hellohuigong@gmail.com>
2023-04-24 14:46:36 +0200

    init frontend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <button className="rounded-full border px-3 py-1 bg-teal-400 font-bold m-2 text-white hover:bg-sky-700 ">
+        click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
